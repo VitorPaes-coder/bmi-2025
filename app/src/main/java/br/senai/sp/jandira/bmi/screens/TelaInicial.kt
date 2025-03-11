f2b2aae630c52284c840f4ed2f3ecbdebad07a44
VITOR PAES RODRIGUES <24122327@dsn127m1242373.cfp127aluno.local>
2025-03-11 12:26:34 -0300

    tudo ok

--- app/src/main/java/br/senai/sp/jandira/bmi/screens/TelaInicial.kt
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/TelaInicial.kt
@@ -9,20 +9,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AppBlocking
 import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -30,34 +26,48 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import br.senai.sp.jandira.bmi.R
 
 @Composable
-fun TelaInicial(modifier: Modifier = Modifier) {
-    var nomeState = remember {
+fun TelaInicial(navController: NavHostController?) {
 
+    var nomeState = remember {
         mutableStateOf(value = "")
     }
+
+    var isErrorState = remember {
+        mutableStateOf(value = false)
+    }
+
+    var errorMessageState = remember {
+        mutableStateOf(value = "")
+    }
+
+    var context = LocalContext.current
+
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(
                 brush = Brush.linearGradient(
                     listOf(
-                        Color(0xFFFFC107),
-                        Color(0xFFF44336)
+                        Color(0xFF5608A4),
+                        Color(0xFFBA88FF)
                     )
                 )
-            ),
-        contentAlignment = Alignment.BottomCenter
+            )
     ) {
+
         Column(
             modifier = Modifier
                 .fillMaxSize(),
@@ -65,8 +75,8 @@ fun TelaInicial(modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.SpaceBetween
         ) {
             Image(
-                painter = painterResource(id = R.drawable.logo01),
-                contentDescription = stringResource(id = R.string.logo_description),
+                painter = painterResource(id = R.drawable.logo2),
+                contentDescription = stringResource(R.string.logo_description),
                 modifier = Modifier
                     .padding(top = 32.dp)
             )
@@ -79,19 +89,18 @@ fun TelaInicial(modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .fillMaxWidth()
                     .height(450.dp),
-                shape = RoundedCornerShape(
-                    topStart = 50.dp,
-                    topEnd = 50.dp
-                )
+                shape = RoundedCornerShape(48.dp, 48.dp)
             ) {
-                Column(
+                Column (
                     modifier = Modifier
                         .fillMaxSize()
                         .padding(32.dp),
                     verticalArrangement = Arrangement.SpaceBetween,
                     horizontalAlignment = Alignment.End
-                ) {
-                    Column {
+                ){
+                    Column (
+                        modifier = Modifier
+                    ){
                         Text(
                             text = stringResource(R.string.your_name),
                             fontSize = 22.sp,
@@ -99,28 +108,49 @@ fun TelaInicial(modifier: Modifier = Modifier) {
                         )
                         OutlinedTextField(
                             value = nomeState.value,
-                            onValueChange = { nome ->
-                                nomeState.value = nome
+                            onValueChange = {
+                                nomeState.value = it
                             },
                             modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(top = 8.dp),
-                            shape = RoundedCornerShape(12.dp),
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(16.dp),
                             label = {
                                 Text(
-                                    stringResource(R.string.your_name_here)
+                                    text = stringResource(R.string.your_name_here)
                                 )
                             },
                             leadingIcon = {
                                 Icon(
-                                    imageVector = Icons.Default.Email,
+                                    imageVector = Icons.Default.AppBlocking,
                                     contentDescription = "",
-                                    tint = Color(0xFFEC7857)
+                                    tint = Color(0xFF5608A4)
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Text,
+                                capitalization = KeyboardCapitalization.Characters
+                            ),
+                            isError = isErrorState.value,
+                            supportingText = {
+                                Text(
+                                    text = errorMessageState.value,
+                                    color = Color.Red
                                 )
                             }
+
                         )
                     }
-                    Button(onClick = {}) {
+
+                    Button(
+                        onClick = {
+                            if (nomeState.value.length < 3){
+                                isErrorState.value = true
+                                errorMessageState.value = context.getString(R.string.support_name)
+                            }else {
+                                navController?.navigate("user_data")
+                            }
+                        }
+                    ) {
                         Text(
                             text = stringResource(R.string.next)
                         )
@@ -129,6 +159,7 @@ fun TelaInicial(modifier: Modifier = Modifier) {
                             contentDescription = ""
                         )
                     }
+
                 }
             }
         }
@@ -138,5 +169,5 @@ fun TelaInicial(modifier: Modifier = Modifier) {
 @Preview(showSystemUi = true)
 @Composable
 private fun TelaInicialPreview() {
-    TelaInicial()
+    TelaInicial(null)
 }
---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/UserDataScreen.kt
@@ -0,0 +1,330 @@
+package br.senai.sp.jandira.bmi.screens
+
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Balance
+import androidx.compose.material.icons.filled.Height
+import androidx.compose.material.icons.filled.Numbers
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun UserDataScreen(modifier: Modifier = Modifier) {
+
+    var ageState = remember {
+        mutableStateOf(value = "")
+    }
+    var weightState = remember {
+        mutableStateOf(value = "")
+    }
+    var heightState = remember {
+        mutableStateOf(value = "")
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color(0xFF8BC34A),
+                        Color(0xBE4CAF50)
+                    )
+                )
+            )
+    ) {
+        Column (
+            modifier = Modifier
+                .fillMaxSize(),
+
+            verticalArrangement = Arrangement.Bottom
+        ){
+
+            Text(
+                text = stringResource(R.string.hi_text),
+                fontSize = 26.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier
+                    .padding(16.dp, 65.dp, 0.dp, 0.dp)
+            )
+
+            Card (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(705.dp),
+                shape = RoundedCornerShape(40.dp, 40.dp)
+            ){
+
+                Column (
+                    modifier = Modifier
+                        .padding(32.dp)
+                        .weight(1f)
+                ){
+
+                    Row (
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 30.dp)
+                    ){
+
+                        Column (
+                            modifier = Modifier
+                                .weight((1f)),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+
+                        ){
+
+                            Card (
+                                shape = CircleShape,
+                                modifier = Modifier
+                                    .size(120.dp),
+                                border = BorderStroke(
+                                    3.dp,
+                                    brush = Brush.linearGradient(
+                                            listOf(
+                                                Color(0xFF497CA3),
+                                                Color(0xFFC4D9F5)
+                                            )
+                                    )
+                                )
+                            ){
+
+                                Image(
+                                    painter = painterResource(id = R.drawable.man_icon),
+                                    contentDescription = stringResource(R.string.logo_description),
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                )
+                            }
+
+                            Button(
+
+                                onClick = {},
+                                colors = ButtonDefaults.buttonColors(containerColor = Color(
+                                    0xFF497BA2
+                                )
+                                ),
+                                shape = RoundedCornerShape(60.dp),
+                                modifier = Modifier
+                                    .padding(0.dp, 7.dp, 0.dp, 0.dp)
+                                    .width(130.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.if_male),
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+
+                            }
+                        }
+
+                        Column (
+                            modifier = Modifier
+                                .weight((1f)),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+
+                            Card (
+                                shape = CircleShape,
+                                modifier = Modifier
+                                    .size(120.dp),
+                                border = BorderStroke(
+                                    3.dp,
+                                    brush = Brush.linearGradient(
+                                        listOf(
+                                            Color(0xFFE0B410),
+                                            Color(0xFFE44A65)
+                                        )
+                                    )
+                                )
+                            ){
+
+                                Image(
+                                    painter = painterResource(id = R.drawable.woman_icon),
+                                    contentDescription = stringResource(R.string.logo_description),
+                                    modifier = Modifier
+                                        .fillMaxSize()
+                                )
+                            }
+
+                            Button(
+
+                                onClick = {},
+                                colors = ButtonDefaults.buttonColors(containerColor = Color(
+                                    0xFFE0B410
+                                )
+                                ),
+                                shape = RoundedCornerShape(60.dp),
+                                modifier = Modifier
+                                    .padding(0.dp, 7.dp, 0.dp, 0.dp)
+                                    .width(130.dp)
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.if_female),
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                            }
+                        }
+                    }
+
+                    Column (
+                        modifier = Modifier
+                            .padding(0.dp, 10.dp),
+                        verticalArrangement = Arrangement.SpaceBetween
+                    ){
+
+                        OutlinedTextField(
+                            value = ageState.value,
+                            onValueChange = {
+                                ageState.value = it
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(16.dp),
+                            label = {
+                                Text(
+                                    text = stringResource(R.string.age)
+                                )
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Numbers,
+                                    contentDescription = "",
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            ),
+                            colors = OutlinedTextFieldDefaults.colors(
+                                cursorColor = Color(0xFF4CAF50),
+                                unfocusedLabelColor = Color(0xFF4CAF50)
+                            )
+                        )
+                        OutlinedTextField(
+                            value = weightState.value,
+                            onValueChange = {
+                                weightState.value = it
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(16.dp),
+                            label = {
+                                Text(
+                                    text = stringResource(R.string.weight)
+                                )
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Balance,
+                                    contentDescription = "",
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            ),
+                            colors = OutlinedTextFieldDefaults.colors(
+                                cursorColor = Color(0xFF4CAF50),
+                                unfocusedLabelColor = Color(0xFF4CAF50)
+                            )
+                        )
+                        OutlinedTextField(
+                            value = heightState.value,
+                            onValueChange = {
+                                heightState.value = it
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            shape = RoundedCornerShape(16.dp),
+                            label = {
+                                Text(
+                                    text = stringResource(R.string.height)
+                                )
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Height,
+                                    contentDescription = "",
+                                )
+                            },
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            ),
+                            colors = OutlinedTextFieldDefaults.colors(
+                                cursorColor = Color(0xFF4CAF50),
+                                unfocusedLabelColor = Color(0xFF4CAF50)
+                            )
+                        )
+
+
+                    }
+
+                    Button(
+                        onClick = {},
+                        colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF4CAF50)
+                        ),
+                        shape = RoundedCornerShape(12.dp),
+                        modifier = Modifier
+                            .padding(0.dp, 7.dp, 0.dp, 0.dp)
+                            .fillMaxWidth()
+                            .height(52.dp)
+                    ) {
+                        Text(
+                            text = stringResource(R.string.calculate),
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showSystemUi = true)
+@Composable
+private fun UserDataScreenPreview() {
+    UserDataScreen()
+}
